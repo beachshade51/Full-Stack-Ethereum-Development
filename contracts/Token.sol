ace06025191dc26e0bc353671172a50785bfaab7
Sidharth P <sidharthp@phaethon.tech>
2023-02-18 17:48:40 +0530

    add token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,22 @@
+import "hardhat/console.sol";
+
+contract Token {
+    string public name = "sid's test token";
+    string public symbol = "STT";
+    uint256 public totalSupply = 1000000;
+    mapping(address => uint256) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "not enough token");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
